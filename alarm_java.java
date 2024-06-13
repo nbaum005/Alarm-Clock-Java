c4dc95dba35499f9d469b47a9aedac6ad1cdf384
nbaum005 <158380318+nbaum005@users.noreply.github.com>
2024-06-12 23:16:32 -0500

    Update alarm_java.java
    Added countdown timer to the set alarm

--- alarm_java.java
+++ alarm_java.java
@@ -3,31 +3,68 @@ package com.company;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.Date;
 
 public class alarm_java {
     public static void main(String[] args) {
-            try{
-                Scanner sc= new Scanner(System.in);
-                System.out.println("Enter the time that in HH:mm a format where HH(hour):mm(minutes) a(pm or am):");
-                String time_set= sc.nextLine();
-                System.out.println("Your alarm is now set for: "+time_set+"...");
-                Boolean flag=true;
-                while(flag){	//Consider always a true condition.
-                    String current_Time = new SimpleDateFormat("HH:mm a").format(new Date());	//current time in HH(hour):mm(minutes):a(pm or am)
-                    boolean x = current_Time.equals(time_set);	//Equating the exact time to the time entered by the user**.
-                    if(x==true){
-                        System.out.println("Wake up it's: "+ time_set);
-                        break;	//Using break to jump out of the loop as soon as the alarm starts ringing.
-                    }
-                    else
-                        continue;
+        try{
+            Scanner sc= new Scanner(System.in);
+            System.out.print("Enter your alarm time in 24 hour (military) time \"hh:mm:ss AM/PM\" format: ");
+            String time_set= sc.nextLine();
+            System.out.println("Your alarm is now set for: "+time_set+"...");
+            Boolean flag=true;
+            while(flag){	//Consider always a true condition.
+                String currentTimeWithSeconds = new SimpleDateFormat("HH:mm:ss a").format(new Date());
+                boolean timesMatch = currentTimeWithSeconds.equals(time_set);	//Equating the exact time to the time entered by the user**.
+                Thread.sleep(1000);
 
+                //System.out.println("Time remaining: " + (time/1000)%60);
+                int currentHours = Integer.parseInt(currentTimeWithSeconds.substring(0, 2));
+                int currentMinutes = Integer.parseInt(currentTimeWithSeconds.substring(3, 5));
+                int currentSeconds = Integer.parseInt(currentTimeWithSeconds.substring(6, 8));
+
+                int setHours = Integer.parseInt(time_set.substring(0,2));
+                int setMinutes = Integer.parseInt(time_set.substring(3,5));
+                int setSeconds = Integer.parseInt(time_set.substring(6,8));
+
+                int countdownHours = setHours-currentHours;
+                int countdownMinutes = setMinutes-currentMinutes;
+                int countdownSeconds = setSeconds-currentSeconds;
+
+                if(countdownHours<0)
+                    countdownHours = countdownHours + 24;
+                if(countdownHours<1 && setMinutes<currentMinutes)
+                    countdownHours = countdownHours + 24;
+                if(countdownMinutes<0)
+                    countdownMinutes = countdownMinutes + 60;
+                if(setMinutes<=currentMinutes && countdownMinutes != 60 && countdownSeconds != 0)
+                    countdownHours--;
+                if(countdownSeconds<=60 && countdownSeconds != 0 )
+                    countdownMinutes--;
+                if(countdownMinutes<0)
+                    countdownMinutes = countdownMinutes + 60;
+                if(countdownSeconds !=0)
+                    countdownSeconds = countdownSeconds + 60;
+                if(countdownHours<0)
+                    countdownHours = countdownHours + 24;
+
+
+                System.out.println("Time left: " + (countdownHours) + " hours, " + (countdownMinutes) + " minutes, and " + (countdownSeconds) + " seconds.");
+
+                if(timesMatch==true || (countdownHours==0 && countdownMinutes==0 && countdownSeconds==0)){
+                    System.out.println();
+                    System.out.println("Wake up! It's: "+ time_set);
+                    break;	//Using break to jump out of the loop as soon as the alarm starts ringing.
                 }
-                flag=false;
+                else
+                    continue;
+
             }
-            catch(Exception e){
-                System.out.println("Check the details you entered again!");
-            }
+            flag=false;
         }
+        catch(Exception e){
+            System.out.println("Check the details you entered again!");
+        }
+    }
 
 }
